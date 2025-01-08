2e8ce9a11498749c5b30475b57ac4f561eed1a66
latoRJ <pilotbyjames@gamil.com>
2025-01-08 17:06:17 +0800

    Final Na Gyud

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::with(['category', 'colors'])->paginate(5c);// Eager loading for optimization
+        $products = Product::with(['category', 'colors'])->paginate(5);// Eager loading for optimization
         $categories = Category::all(); // Fetch all categories
         return view('admin.productstocks', compact('products', 'categories'));
     }
